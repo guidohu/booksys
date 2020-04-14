1e09e507912ff81ab8a20eda9d20c2cf50dc2877
guidohu <ghungerbuehler@gmail.com>
2020-04-14 21:09:43 +0200

    change format of password reset

--- src/res/view_password_reset_email.vue
+++ src/res/view_password_reset_email.vue
@@ -23,25 +23,21 @@
                             </div>
                             <!-- email -->
                             <div class="row">
-                                <div class="col-sm-1 col-xs-1"></div>
-                                <div class="col-sm-3 col-xs-3">
+                                <div class="col-sm-2 col-xs-2">
                                     <div>
                                         <label class="label-sm" for="">Email</label>
                                     </div>
                                 </div>
-                                <div class="cold-sm-7 col-xs-7">
+                                <div class="cold-sm-10 col-xs-10">
                                     <input v-model="email" type="text" class="form-control form-control-white input-sm" placeholder=""/>
                                 </div>
-                                <div class="col-sm-1 col-xs-1"></div>
                             </div>
                             <!-- captcha -->
                             <div class="row row-top-padded">
-                                <div class="col-sm-1 col-xs-1"></div>
-                                <div class="col-sm-3 col-xs-3"></div>
-                                <div class="col-sm-7 col-xs-7">
+                                <div class="col-sm-2 col-xs-2"></div>
+                                <div class="col-sm-10 col-xs-10">
                                     <div id="captcha"></div>
                                 </div>
-                                <div class="col-sm-1 col-xs-1"></div>
                             </div>
                         </div>
                         <div v-if="sending==true">
