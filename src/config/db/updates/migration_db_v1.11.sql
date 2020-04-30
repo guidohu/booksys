1b240a2678c71fb46c379750df0f7bedb1239790
guidohu <ghungerbuehler@gmail.com>
2020-04-30 17:22:19 +0200

    Update changes and bump version

--- src/config/db/updates/migration_db_v1.11.sql
+++ src/config/db/updates/migration_db_v1.11.sql
@@ -2,4 +2,4 @@
 ALTER TABLE user ADD column deleted tinyint(1) DEFAULT '0' AFTER comment;
 
 -- set new version in configuration
-REPLACE INTO configuration (property, value) VALUES ("schema.version", "1.11");
+REPLACE INTO configuration (property, value) VALUES ("schema.version", "1.12");
