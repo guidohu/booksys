71a43b804eb9dc0d9ad4088559f2fb0e407069af
guidohu <ghungerbuehler@gmail.com>
2020-05-01 13:44:12 +0200

    Release v1.14

--- src/classes/Configuration.php
+++ src/classes/Configuration.php
@@ -79,7 +79,7 @@ class Configuration{
 
 		// set hard defaults
 		$this->login_page          = '/index.html';
-		$this->version             = "1.13";
+		$this->version             = "1.14";
 		$this->required_db_version = "1.13";
 	}
 
