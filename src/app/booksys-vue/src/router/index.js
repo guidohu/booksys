507aebf0022226eff50b45288936ebe41a8705d1
guidohu <ghungerbuehler@gmail.com>
2020-09-04 22:39:32 +0200

    add default route to forward to /login

--- src/app/booksys-vue/src/router/index.js
+++ src/app/booksys-vue/src/router/index.js
@@ -9,11 +9,10 @@ import Info from '../views/Info.vue'
 Vue.use(VueRouter)
 
 const routes = [
-  // {
-  //   path: '/',
-  //   name: 'Login',
-  //   component: Login
-  // },
+  {
+    path: '/',
+    redirect: '/login'
+  },
   {
     path: '/login',
     name: 'Login',
@@ -39,14 +38,6 @@ const routes = [
     name: 'Info',
     component: Info
   }
-  // {
-    // path: '/about',
-    // name: 'About',
-    // // route level code-splitting
-    // // this generates a separate chunk (about.[hash].js) for this route
-    // // which is lazy-loaded when the route is visited.
-    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  // }
 ]
 
 const router = new VueRouter({
