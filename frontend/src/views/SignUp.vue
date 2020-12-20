f0e9711fbc3f122cc9ac2bcb2c0d1f6db72f80c3
guidohu <ghungerbuehler@gmail.com>
2020-12-20 14:51:29 +0100

    use user sign up form inside user sign up view

--- frontend/src/views/SignUp.vue
+++ frontend/src/views/SignUp.vue
@@ -7,191 +7,27 @@
     hide-header-close
     visible
   >
-    <b-row v-if="isSignedUp==false" class="text-left mb-4">
-      <b-col cols="1" class="d-none d-sm-block"></b-col>
-      <b-col cols="12" sm="10">
-        Please fill the form below to create your own personal account.
-      </b-col>
-      <b-col cols="1" class="d-none d-sm-block"></b-col>
-    </b-row>
-    <b-form v-if="isSignedUp==false" @submit="save">
-      <b-row class="text-left">
+    <div v-if="isSignedUp==false">
+      <b-row class="text-left mb-4">
         <b-col cols="1" class="d-none d-sm-block"></b-col>
         <b-col cols="12" sm="10">
-          <!-- Email -->
-          <b-form-group
-            id="email"
-            label="Email"
-            label-for="email-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="email-input"
-              v-model="form.email"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Password -->
-          <b-form-group
-            id="password"
-            label="Password"
-            label-for="password-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="password-input"
-              v-model="form.password"
-              type="password"
-            />
-          </b-form-group>
-          <!-- Password Confirm -->
-          <b-form-group
-            id="password-confirm"
-            label="Confirm"
-            label-for="password-confirm-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="password-confirm-input"
-              v-model="form.passwordConfirm"
-              type="password"
-            />
-          </b-form-group>
-          <hr/>
-          <!-- First Name -->
-          <b-form-group
-            id="first-name"
-            label="First Name"
-            label-for="first-name-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="first-name-input"
-              v-model="form.firstName"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Surname -->
-          <b-form-group
-            id="last-name"
-            label="Surname"
-            label-for="last-name-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="last-name-input"
-              v-model="form.lastName"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Street / No -->
-          <b-form-group
-            id="street"
-            label="Street / No"
-            label-for="street-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="street-input"
-              v-model="form.street"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Zip Code -->
-          <b-form-group
-            id="zip-name"
-            label="Zip Code"
-            label-for="zip-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="zip-input"
-              v-model="form.zip"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Place / City -->
-          <b-form-group
-            id="city"
-            label="Place / City"
-            label-for="city-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="city-input"
-              v-model="form.city"
-              type="text"
-            />
-          </b-form-group>
-          <!-- Phone -->
-          <b-form-group
-            id="phone"
-            label="Phone"
-            label-for="phone-input"
-            description=""
-            label-cols="3"
-          >
-            <b-form-input
-              id="phone-input"
-              v-model="form.phone"
-              type="text"
-            />
-          </b-form-group>
-          <hr/>
-          <!-- License -->
-          <b-row class="text-left align-middle"
-          >
-            <b-col cols="9">
-              I have a driver's license for boats.
-            </b-col>
-            <b-col cols="3" class="text-right">
-              <toggle-button 
-                id="license-toggle"
-                :value="form.license"
-                sync
-                @change="licenseToggleHandler"
-                color="#17a2b8"
-                :width="toggleWidth"
-                :labels="{checked: 'Yes', unchecked: 'No'}"
-              />
-            </b-col>
-          </b-row>
-          <!-- Own risk -->
-          <b-row class="text-left align-middle mt-3"
-          >
-            <b-col cols="9">
-              I know the boat community and I perform the activities on my own risk.
-            </b-col>
-            <b-col cols="3" class="text-right">
-              <toggle-button 
-                id="own-risk-toggle"
-                :value="form.ownRisk"
-                sync
-                @change="ownRiskToggleHandler"
-                color="#17a2b8"
-                :width="toggleWidth"
-                :labels="{checked: 'Yes', unchecked: 'No'}"
-              />
-            </b-col>
-          </b-row>
-          <!-- Captcha -->
-          <b-row v-if="getRecaptchaKey != null" class="mt-3">
-            <b-col cols="12">
-              <vue-recaptcha :sitekey="getRecaptchaKey" :loadRecaptchaScript="true" @verify="verifiedHandler"></vue-recaptcha>
-            </b-col>
-          </b-row>
+          Please fill the form below to create your own personal account.
         </b-col>
         <b-col cols="1" class="d-none d-sm-block"></b-col>
       </b-row>
-    </b-form>
+      <user-sign-up
+        :user-data="form"
+        :show-disclaimer="true"
+      />
+      <b-row v-if="getRecaptchaKey != null" class="mt-3">
+        <b-col cols="1" class="d-none d-sm-block"></b-col>
+        <b-col cols="12" sm="10">
+          <vue-recaptcha :sitekey="getRecaptchaKey" :loadRecaptchaScript="true" @verify="recaptchaVerifiedHandler"></vue-recaptcha>
+        </b-col>
+        <b-col cols="1" class="d-none d-sm-block"></b-col>
+      </b-row>
+    </div>
+    <!-- Sign up confirmation -->
     <b-row v-if="isSignedUp==true" class="text-center">
       <b-col cols="1" class="d-none d-sm-block"></b-col>
       <b-col cols="12" sm="10">
@@ -233,16 +69,16 @@
 <script>
 import Vue from 'vue';
 import { mapGetters, mapActions } from 'vuex';
-import { ToggleButton } from 'vue-js-toggle-button';
 import WarningBox from '@/components/WarningBox';
+import UserSignUp from '@/components/forms/UserSignUp';
 import VueRecaptcha from 'vue-recaptcha';
 import User from '@/api/user';
 
 export default Vue.extend({
   name: "SignUp",
   components: {
-    ToggleButton,
     WarningBox,
+    UserSignUp,
     VueRecaptcha
   },
   data() {
@@ -266,13 +102,7 @@ export default Vue.extend({
     ...mapActions('configuration', [
       'queryRecaptchaKey'
     ]),
-    licenseToggleHandler: function(){
-      this.form.license = !this.form.license;
-    },
-    ownRiskToggleHandler: function(){
-      this.form.ownRisk = !this.form.ownRisk;
-    },
-    verifiedHandler: function(response){
+    recaptchaVerifiedHandler: function(response){
       this.form.recaptchaResponse = response;
     },
     validateForm: function() {
