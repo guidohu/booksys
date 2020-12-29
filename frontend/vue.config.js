799f09cbd3f01522f470499bf7dfa6ce044541b5
guidohu <ghungerbuehler@gmail.com>
2020-12-29 11:48:07 +0100

    package version updates and correct configuration of assetsDir in vue.config

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  // assetsDir: './src/assets',
+  assetsDir: './src/assets',
   outputDir: 'dist',
   publicPath: '/',
   // indexPath: 'index.html',
