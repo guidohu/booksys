8caa6a4774969bdcbf2768830f444851ddd3cf7c
guidohu <ghungerbuehler@gmail.com>
2022-03-19 18:32:09 +0100

    add icons again

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -7,6 +7,7 @@ import { store } from "./store";
 // Bootstrap
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.esm";
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 // Override bootstrap styling
 import "@/assets/bootstrap/css/bootstrap-theme-bc.css";
