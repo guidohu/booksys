47e1f1d9a48ada20d637b41ef26d45b85a284b4b
guidohu <ghungerbuehler@gmail.com>
2023-07-29 13:52:43 +0200

    fix log book api

--- backend/api/v2/database/log.go
+++ backend/api/v2/database/log.go
@@ -26,36 +26,36 @@ func (d *DBMysql) GetLogs() ([]Log, error) {
 		slog.Warn("Currency is not a safe and valid string to build the SQL statement for retrieving logs")
 		return []Log{}, fmt.Errorf("currency is no valid value log text cannot be built")
 	}
-	query := fmt.Sprintf(`SELECT c_log.id as id, c_log.type as type, c_log.time as time, c_log.log as log FROM (
+	query := fmt.Sprintf(`SELECT c_log.id as id, c_log.type as type, c_log.time as time, c_log.log_message as log_message FROM (
 		SELECT h.id as id, "heat" as type, h.timestamp as time, 
 			   CONCAT(u.first_name COLLATE utf8_general_ci, ' ', u.last_name, 
 					  ' was riding for ', FORMAT(h.duration_s/60,1), 
-					  ' min and ', FORMAT(h.cost_chf, 2), ' %s') as log 
+					  ' min and ', FORMAT(h.cost_chf, 2), ' %s') as log_message 
 			FROM user u, heat h 
 			WHERE u.id = h.user_id AND h.comment IS NULL
 		UNION ALL
 		SELECT h.id as id, "heat" as type, h.timestamp as time, 
 			   CONCAT(u.first_name COLLATE utf8_general_ci, ' ', u.last_name, 
 					  ' was riding for ', FORMAT(h.duration_s/60,1), 
-					  ' min and ', FORMAT(h.cost_chf, 2), ' %s (Comment: ', h.comment, ')') as log 
+					  ' min and ', FORMAT(h.cost_chf, 2), ' %s (Comment: ', h.comment, ')') as log_message 
 			FROM user u, heat h 
 			WHERE u.id = h.user_id AND NOT h.comment IS NULL
 		UNION ALL
 		SELECT p.id as id, "payment" as type, p.timestamp as time, 
 			   CONCAT(u.first_name COLLATE utf8_general_ci, ' ', u.last_name, 
-					  ' paid for sessions ', FORMAT(p.amount_chf, 2), ' %s') as log 
+					  ' paid for sessions ', FORMAT(p.amount_chf, 2), ' %s') as log_message 
 			FROM user u, payment p 
 			WHERE u.id = p.user_id AND p.type_id = 4
 		UNION ALL
 		SELECT bf.id as id, "boat_fuel" as type, bf.timestamp as time, 
 			   CONCAT(u.first_name COLLATE utf8_general_ci, ' ', u.last_name, ' added ', 
-					  FORMAT(bf.liters, 2), 'L fuel for ', FORMAT(cost_chf, 2), ' %s') as log 
+					  FORMAT(bf.liters, 2), 'L fuel for ', FORMAT(cost_chf, 2), ' %s') as log_message 
 			FROM user u, boat_fuel bf 
 			WHERE bf.user_id = u.id AND bf.contributes_to_balance = 1
 		UNION ALL
 		SELECT bf.id as id, "boat_fuel" as type, bf.timestamp as time, 
 			   CONCAT(u.first_name COLLATE utf8_general_ci, ' ', u.last_name, ' added ', 
-					  FORMAT(bf.liters, 2), 'L fuel for ', FORMAT(cost_chf, 2), ' %s (on credit)') as log 
+					  FORMAT(bf.liters, 2), 'L fuel for ', FORMAT(cost_chf, 2), ' %s (on credit)') as log_message 
 			FROM user u, boat_fuel bf 
 			WHERE bf.user_id = u.id AND bf.contributes_to_balance = 0
 	  ) as c_log ORDER BY time DESC`, currency.Value, currency.Value, currency.Value, currency.Value, currency.Value)
