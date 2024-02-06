f908d9b4a7e6a4e68436e4f2655bef40aa9b978e
guidohu <ghungerbuehler@gmail.com>
2024-02-06 20:52:29 +0100

    update external libraries

--- frontend/src/components/styling/OverlaySpinner.vue
+++ frontend/src/components/styling/OverlaySpinner.vue
@@ -10,7 +10,7 @@
 <script>
 import { defineComponent } from "vue";
 import Loading from "vue-loading-overlay";
-import "vue-loading-overlay/dist/vue-loading.css";
+import 'vue-loading-overlay/dist/css/index.css';
 
 export default defineComponent({
   name: "OverlaySpinner",
