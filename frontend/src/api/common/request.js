8c021b84479af154c95c2bd1fe0b9071a575658c
guidohu <ghungerbuehler@gmail.com>
2025-06-08 20:24:02 +0200

    Improve error handling for request handling

--- frontend/src/api/common/request.js
+++ frontend/src/api/common/request.js
@@ -7,21 +7,35 @@ export default class Request {
         cache: "no-cache",
       })
         .then((response) => {
-          response
-            .json()
-            .then((data) => {
-              console.debug("GET", url, "response data:", data);
-              if (data.ok || data.status.ok) {
-                resolve(data.data);
-              } else {
-                console.warn("GET", url, "response not ok, due to:", data.msg);
-                reject([data.msg]);
+          if (response.status != 200) {
+            // handle a few specific error messages
+            response.text().then((bodyText) => {
+              if (response.status === 500 && bodyText.includes("Proxy erro")) {
+                reject(["Internal Server Error: Your request cannot be proxied to the backend. Is the API backend reachable?"]);
               }
+              reject(["Error: Your request could not be handled. There is an issue with your request or with the backend."]);
             })
             .catch((error) => {
-              console.warn("GET", url, "cannot parse server response", error);
-              reject([error]);
-            });
+              console.log("Internal Server Error, cannot get response body", error);
+              reject(["Error: Your request could not be handled. There is an issue with your request or with the backend."]);
+            })
+          } else {
+            response
+              .json()
+              .then((data) => {
+                console.debug("GET", url, "response data:", data);
+                if (data.ok || data.status.ok) {
+                  resolve(data.data);
+                } else {
+                  console.warn("GET", url, "response not ok, due to:", data.msg);
+                  reject([data.msg]);
+                }
+              })
+              .catch((error) => {
+                console.warn("GET", url, "cannot parse server response", error);
+                reject([error]);
+              });
+          }
         })
         .catch((error) => {
           console.error("GET", url, "request failed", error);
@@ -39,21 +53,35 @@ export default class Request {
         body: JSON.stringify(payload),
       })
         .then((response) => {
-          response
-            .json()
-            .then((data) => {
-              console.debug("POST", url, "response data:", data);
-              if (data.ok) {
-                resolve(data.data);
-              } else {
-                console.warn("POST", url, "response not ok, due to:", data.msg);
-                reject([data.msg]);
+          if (response.status != 200) {
+            // handle a few specific error messages
+            response.text().then((bodyText) => {
+              if (response.status === 500 && bodyText.includes("Proxy erro")) {
+                reject(["Internal Server Error: Your request cannot be proxied to the backend. Is the API backend reachable?"]);
               }
+              reject(["Error: Your request could not be handled. There is an issue with your request or with the backend."]);
             })
             .catch((error) => {
-              console.warn("POST", url, "cannot parse server response", error);
-              reject([error]);
-            });
+              console.log("Internal Server Error, cannot get response body", error);
+              reject(["Error: Your request could not be handled. There is an issue with your request or with the backend."]);
+            })
+          } else {
+            response
+              .json()
+              .then((data) => {
+                console.debug("POST", url, "response data:", data);
+                if (data.ok) {
+                  resolve(data.data);
+                } else {
+                  console.warn("POST", url, "response not ok, due to:", data.msg);
+                  reject([data.msg]);
+                }
+              })
+              .catch((error) => {
+                  console.warn("POST", url, "cannot parse server response", error);
+                  reject([error]);
+              });
+          }
         })
         .catch((error) => {
           console.warn("POST", url, "request failed", error);
