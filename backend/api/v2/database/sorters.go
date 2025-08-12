065ea63c8c1157dc1663f710faedb930af8d2dd0
guidohu <ghungerbuehler@gmail.com>
2025-08-12 22:25:43 +0200

    Add an additional sorter.

--- backend/api/v2/database/sorters.go
+++ backend/api/v2/database/sorters.go
@@ -5,3 +5,9 @@ type SessionByTime []Session
 func (a SessionByTime) Len() int           { return len(a) }
 func (a SessionByTime) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 func (a SessionByTime) Less(i, j int) bool { return a[i].StartTime.Before(a[j].StartTime) }
+
+type SessionByTimeDesc []Session
+
+func (a SessionByTimeDesc) Len() int           { return len(a) }
+func (a SessionByTimeDesc) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
+func (a SessionByTimeDesc) Less(i, j int) bool { return a[i].StartTime.After(a[j].StartTime) }
